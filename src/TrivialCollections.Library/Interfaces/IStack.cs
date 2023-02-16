667a6260fb3f881a374f359b360987bf3fa0d926
Vladimir <vovikdoc@gmail.com>
2023-02-17 01:47:50 +0700

    preview

---
+++ src/TrivialCollections.Library/Interfaces/IStack.cs
@@ -0,0 +1,8 @@
+﻿namespace TrivialCollections.Library.Interfaces;
+
+public interface IStack<T>
+{
+    void Push(T? item);
+    T? Pop();
+    T? Peek();
+}
---
+++ src/TrivialCollections.Tests/Program.cs
@@ -0,0 +1,76 @@
+﻿using TrivialCollections.Library;
+using TrivialCollections.Library.Interfaces;
+
+var test1 = new Test { Data = new IntWrapper { Number = 1 } };
+var test2 = new Test { Data = null };
+var test3 = new Test { Data = new IntWrapper { Number = 3 } };
+
+Console.WriteLine("STACK test:");
+
+IStack<Test> stack = new TrivialStack<Test>();
+stack.Push(test1);
+stack.Push(test2);
+stack.Push(test3);
+
+Console.WriteLine("Peek: " + stack.Peek());
+Console.WriteLine("Peek: " + stack.Peek());
+
+Console.WriteLine("*****");
+
+Console.WriteLine("Pop: " + stack.Pop());
+Console.WriteLine("Pop: " + stack.Pop());
+Console.WriteLine("Pop: " + stack.Pop());
+
+Console.WriteLine("*****");
+
+try
+{
+    Console.WriteLine("Pop: " + stack.Pop());
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine(e);
+    Console.WriteLine("Так было задумано)))");
+}
+
+Console.WriteLine("\nQUEUE test:");
+
+IQueue<Test> queue = new TrivialQueue<Test>();
+queue.Enqueue(test1);
+queue.Enqueue(test2);
+queue.Enqueue(test3);
+
+Console.WriteLine("Peek: " + queue.Peek());
+Console.WriteLine("Peek: " + queue.Peek());
+
+Console.WriteLine("*****");
+
+Console.WriteLine("Dequeue: " + queue.Dequeue());
+Console.WriteLine("Dequeue: " + queue.Dequeue());
+Console.WriteLine("Dequeue: " + queue.Dequeue());
+
+Console.WriteLine("*****");
+
+try
+{
+    Console.WriteLine("Dequeue: " + queue.Dequeue());
+}
+catch (InvalidOperationException e)
+{
+    Console.WriteLine(e);
+    Console.WriteLine("Так было задумано)))");
+}
+
+Console.ReadLine();
+
+file class Test
+{
+    public IntWrapper? Data { get; init; }
+
+    public override string? ToString() => Data is null ? "no data" : Data.Number.ToString();
+}
+
+internal class IntWrapper
+{
+    public int Number { get; init; }
+}
